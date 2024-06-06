23f994ac4675a1ea59b1b56e2d88a477398c4540
matrixxx602 <159135973+Matrixxx602@users.noreply.github.com>
2024-06-06 16:55:00 +0200

    Dodanie projektu.

---
+++ Colors/DefaultSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Colors
+{
+    public static class DefaultSettings
+    {
+        public static Color ReadSettings()
+        {
+
+            Properties.Settings settings = new Properties.Settings();
+            Color color = new Color()
+            {
+                A = 255,
+                R = settings.R,
+                G = settings.G,
+                B = settings.B
+            };
+            return color;
+        }
+
+        public static void SaveSettings(Color color)
+        {
+            Properties.Settings settings = new Properties.Settings();
+            settings.R = color.R;
+            settings.G = color.G;
+            settings.B = color.B;
+            settings.Save();
+        }
+        
+
+    }
+}
---
+++ Colors/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Colors.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Colors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Color color = new Color();
+            color = DefaultSettings.ReadSettings();
+            Rectangle.Fill = new SolidColorBrush(color);
+            SliderR.Value = color.R;
+            SliderG.Value = color.G;
+            SliderB.Value = color.B;
+        }
+
+        
+
+        private void Sliders_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color color = Color.FromRgb(
+                (byte)SliderR.Value, 
+                (byte)SliderG.Value, 
+                (byte)SliderB.Value);
+            ColorRectangle = color;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) Close();
+        }
+
+        private Color ColorRectangle
+        {
+            get
+            {
+                return (Rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (Rectangle.Fill as SolidColorBrush).Color = value;
+            }
+
+        }
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            DefaultSettings.SaveSettings(ColorRectangle);
+        }
+    }
+}
